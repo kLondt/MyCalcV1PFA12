fa339e9a30817dccb8c90dd878630efaa1a106fb
kLondt <137270016+kLondt@users.noreply.github.com>
2023-10-05 10:06:14 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace MyCalcV1PFA12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnclose = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btncal = new System.Windows.Forms.Button();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.txtoper = new System.Windows.Forms.TextBox();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstout = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnclose
+            // 
+            this.btnclose.BackColor = System.Drawing.Color.Crimson;
+            this.btnclose.Location = new System.Drawing.Point(672, 399);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(75, 23);
+            this.btnclose.TabIndex = 0;
+            this.btnclose.Text = "exit";
+            this.btnclose.UseVisualStyleBackColor = false;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.btnclear.Location = new System.Drawing.Point(495, 302);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 1;
+            this.btnclear.Text = "clear ";
+            this.btnclear.UseVisualStyleBackColor = false;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btncal
+            // 
+            this.btncal.BackColor = System.Drawing.Color.Cyan;
+            this.btncal.Location = new System.Drawing.Point(190, 302);
+            this.btncal.Name = "btncal";
+            this.btncal.Size = new System.Drawing.Size(75, 23);
+            this.btncal.TabIndex = 2;
+            this.btncal.Text = "calculate ";
+            this.btncal.UseVisualStyleBackColor = false;
+            this.btncal.Click += new System.EventHandler(this.btncal_Click);
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(109, 173);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(100, 22);
+            this.txtnum2.TabIndex = 3;
+            this.txtnum2.TextChanged += new System.EventHandler(this.txtnum2_TextChanged);
+            // 
+            // txtoper
+            // 
+            this.txtoper.Location = new System.Drawing.Point(109, 109);
+            this.txtoper.Name = "txtoper";
+            this.txtoper.Size = new System.Drawing.Size(100, 22);
+            this.txtoper.TabIndex = 4;
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(109, 51);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(100, 22);
+            this.txtnum1.TabIndex = 5;
+            this.txtnum1.TextChanged += new System.EventHandler(this.txtnum1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Number1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "operation ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "number 2";
+            // 
+            // lstout
+            // 
+            this.lstout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(241)))), ((int)(((byte)(211)))));
+            this.lstout.FormattingEnabled = true;
+            this.lstout.ItemHeight = 16;
+            this.lstout.Location = new System.Drawing.Point(450, 34);
+            this.lstout.Name = "lstout";
+            this.lstout.Size = new System.Drawing.Size(200, 180);
+            this.lstout.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aquamarine;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstout);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.txtoper);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.btncal);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnclose);
+            this.Name = "Form1";
+            this.Text = "my CalcuV1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnclose;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btncal;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.TextBox txtoper;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstout;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCalcV1PFA12
+{
+    public partial class Form1 : Form
+
+    {
+        int FirstNUM;
+        int SeconNUM;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+
+        {
+
+            if (MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+               
+            }
+           
+        }
+
+        private void txtnum1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncal_Click(object sender, EventArgs e)
+        {
+            FirstNUM = int.Parse(txtnum1.Text);
+            SeconNUM = int.Parse(txtnum2.Text);
+            if (txtoper.Text == "+") {  
+                int addition = FirstNUM + SeconNUM;
+                lstout.Items.Add(FirstNUM+""+"+"+""+SeconNUM+"=" +addition);
+            }
+           else if( txtoper.Text=="-")
+            {
+                int subtraction = FirstNUM - SeconNUM;
+                lstout.Items.Add(FirstNUM + "" + "-" + "" + SeconNUM+"=" +subtraction);
+            }
+            else if (txtoper.Text == "*")
+            {
+                int product = FirstNUM * SeconNUM;
+                lstout.Items.Add(FirstNUM + "" + "*"+ "" + SeconNUM +"="+ product);
+
+            }
+            else if (txtoper.Text == "/")
+            {
+                int division = FirstNUM / SeconNUM;
+                lstout.Items.Add(FirstNUM + "" + "/" + "" + SeconNUM + "=" +division);
+
+            }
+           else if (txtoper.Text == "%")
+            {
+                int modules = FirstNUM % SeconNUM;
+                lstout.Items.Add(FirstNUM + "" + "%" + "" + SeconNUM + "=" + modules);
+            }
+            
+            else
+            {
+                lstout.Items.Add("please insert a valid input ");
+            }
+
+        
+
+
+        }
+
+        private void txtnum2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            lstout.Items.Clear();
+            txtnum1.Text = "";
+            txtnum2.Text = "";
+            txtoper.Text = "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
